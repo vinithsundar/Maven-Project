0db459a3d12f98b6fc1371884e70591662eeded7
vinithsundar <vinithsundar360@gmail.com>
2022-07-05 01:38:35 +0530

    Project Into git

---
+++ src/main/java/com/fb/excel/ExcelActions.java
@@ -0,0 +1,18 @@
+package com.fb.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExcelActions {
+	public static void main(String[] args) throws Exception {
+//		
+		File f=new File("C:\\Users\\Administrator\\eclipse-workspace1\\MavenProject\\src\\main\\resources\\ExcelFile\\New Microsoft Office Excel Worksheet.xlsx");
+FileInputStream r=new FileInputStream(f);
+System.out.println("h");
+	 
+	
+	}
+
+}
+ 
